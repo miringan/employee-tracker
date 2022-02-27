88431ef975b5e15ee1b466dd10f9444e6d9aff93
Michael Iringan <miringan@uw.edu>
2022-02-27 01:43:06 -0800

    Significantly updated index.js, all functions now work properly and notes are written.

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// import packages
 const inquire = require('inquirer');
 const db = require('./config/connection');
 const cTable = require('console.table');
@@ -51,94 +52,61 @@ async function viewAllDepartments() {
     const departments = await db.query('SELECT * FROM department');
 
     console.table(departments);
-    // console.log('\n');
+
     run();
 
 }
 
-// view all roles - 
-
+// view all roles 
 async function viewAllRoles() {
 
-    const roles = await db.query('SELECT * FROM roles');
+    const roles = await db.query('SELECT * FROM roles INNER JOIN department ON roles.department_id=department.id');
 
     console.table(roles);
+
     run();
-    // console.log('\n');
 
 }
 
-// view all employees - READ - "SELECT * FROM [table_name]"
+// view all employees 
 async function viewAllEmployees() {
 
-    const employees = await db.query('SELECT * FROM employee');
+    const employees = await db.query('SELECT * FROM employee INNER JOIN roles ON employee.roles_id=roles.id');
 
     console.table(employees);
+
     run();
-    // console.log('\n');
 
 }
 
-// add a department - CREATE - "INSERT INTO [table_name] (col1, col2) VALUES (value1, value2)"
-
+// add a department 
 async function createDepartment() {
     
-    await inquirer.prompt([
-        {
-            type: "input",
-            name: "name",
-            message: "Provide a department name."
-        }
-    ]).then((data) => {
-
-            // console.log('I made it!')
-            const sql =(`INSERT INTO department SET ?`, data.name);
-                // VALUES (${data.name})`;
-            console.log(sql);
-      
+    // Ask the user to provide a department name.
+    inquire
+        .prompt([
+            {
+                type: "input",
+                name: "department_name",
+                message: "Provide a department name."
+            }
+        ])
+        // Take the user's answers and go INSERT them into the 'role' table
+        .then((data) => {
+            console.log(data);
+            const sql = `INSERT INTO department (department_name) 
+                VALUES ("${data.department_name}")`;
             db.query(sql, (err, result) => {
-                if (err) {
-                    res.status(400).json({ error: err.message });
-                    return;
+                if (err) throw err; 
+                console.log('New department added to departments.');
+                if (result) {
+                    run();
                 }
-                res.json({
-                message: 'success',
-                data: body
-                });
-            }).then(() => run());
-
-    })
-    // const departmentInfo = await inquire
-    //     .prompt([
-    //         {
-    //             type: "input",
-    //             name: "name",
-    //             message: "Provide a department name."
-    //         }
-    //     ]);
-    // console.log(departmentInfo);
-    //     // function departmentUpdate(data) {
-    //     //     console.log('I made it!')
-    //     //     const sql =`INSERT INTO department (department_name) 
-    //     //         VALUES (${data.name})`;
-    //     //     const params = roleInformation;
-      
-    //     //     db.query(sql, params, (err, result) => {
-    //     //         if (err) {
-    //     //             res.status(400).json({ error: err.message });
-    //     //             return;
-    //     //         }
-    //     //         res.json({
-    //     //         message: 'success',
-    //     //         data: body
-    //     //         });
-    //     //     });
-    //     // };
-    // // departmentUpdate(data);
+            });
+        })  
 }
 
-// add a role - CREATE -
-
+// add a role 
 async function createRole() {
 
     // SELECT the existing departments out for the 'roles' table
@@ -147,7 +115,7 @@ async function createRole() {
     // .map() the reults from 'roles' to question data for inquirer
     const departmentChoices = departments.map(department => {
         return {
-            name: department.name,
+            name: department.department_name,
             value: department.id
         }
     })
@@ -178,7 +146,6 @@ async function createRole() {
             const sql = `INSERT INTO roles (title, salary, department_id) 
                 VALUES ("${data.title}", ${data.salary}, ${data.department_id})`;
             db.query(sql, (err, result) => {
-                console.log('query has been hit');
                 if (err) throw err; 
                 console.log('New role added to roles.');
                 if (result) {
@@ -188,71 +155,67 @@ async function createRole() {
         })  
 }
 
-// add an employee - CREATE -
+// add an employee 
 async function createEmployee() {
     // grab all roles from roles table and store in an array
     const roles = await db.query('SELECT * FROM roles');
 
+    // .map() the reults from 'roles' to question data for inquirer
     const roleSelection = roles.map(roles => {
         return {
-            title: roles.title,
+            name: roles.title,
             value: roles.id
         }
     })
 
+    console.log(roleSelection);
     // grab managers from employees table and store in an array
     const employees = await db.query('SELECT * FROM employee');
 
-    const managers = [];
-
-    for (var i = 0; i < employees.length; i++) {
-        if (employees[i].roles_id === 1) {
-            managers.push(employees[i]);
+    // .map() the reults from 'employees' to question data for inquirer
+    const managers = employees.map(employees => {
+        return {
+            name: employees.first_name + " " + employees.last_name,
+            value: employees.id
         }
-    }
+    });
 
-    const employeeInfo = await inquire
+    // ask the user for information about the new employee
+    inquire
         .prompt([
             {
                 type: "input",
                 name: "first_name",
-                question: "Provide a first name."
+                message: "Provide a first name."
             },
             {
                 type: "input",
                 name: "last_name",
-                question: "Provide a last name."
+                message: "Provide a last name."
             },
             {
                 type: "list",
                 name: "roles_id",
-                question: "Select the employee's role.",
+                message: "Select the employee's role.",
                 choices: roleSelection
             },
             {
                 type: "list",
                 name: "manager_id",
-                question: "Select the employee's manager.",
+                message: "Select the employee's manager.",
                 choices: managers
             }    
         ]).then((data) => {
             const sql = `INSERT INTO employee (first_name, last_name, roles_id, manager_id) 
-                VALUES (${data.first_name}, ${data.last_name}, ${data.roles_id.value}, ${data.manager_id.manager_id})`;
-            const params = employeeInfo;
-      
-            db.query(sql, params, (err, result) => {
-                if (err) {
-                res.status(400).json({ error: err.message });
-                return;
+                VALUES ("${data.first_name}", "${data.last_name}", ${data.roles_id}, ${data.manager_id})`;
+            db.query(sql, (err, result) => {
+                if (err) throw err; 
+                console.log('New employee added to employees.');
+                if (result) {
+                    run();
                 }
-                res.json({
-                message: 'success',
-                data: body
-                });
             });
         })
-    run();
-
 }
 
 // update an employee
@@ -263,8 +226,7 @@ async function updateEmployeeRole() {
     // put deconstructed employee sql data into an array
     const employeeRoster = employees.map(employees => {
         return {
-            first_name: employees.first_name,
-            last_name: employees.last_name,
+            name: employees.first_name + " " + employees.last_name,
             value: employees.id
         }
     })
@@ -275,50 +237,39 @@ async function updateEmployeeRole() {
     // put deconstructed roles into an array
     const roleSelection = roles.map(roles => {
         return {
-            title: roles.title,
+            name: roles.title,
             value: roles.id
         }
     })
 
-    // grab managers from employees table and store in an array
-    const employeeScan = await db.query('SELECT * FROM employee');
-
-    const managers = [];
-
-    for (var i = 0; i < employeeScan.length; i++) {
-        if (employeeScan[i].roles_id === 1) {
-            managers.push(employeeScan[i]);
-        }
-    }
-
+    // ask the user information regarding what to update on the employee
     const employeeUpdates = await inquire
         .prompt([
             {
                 type: "list",
                 name: "employee",
-                question: "Which employee will be updated?",
+                message: "Which employee will be updated?",
                 choices: employeeRoster
             },
             {
                 type: "list",
                 name: "role",
-                question: "What new role will the employee take?",
+                message: "What new role will the employee take?",
                 choices: roleSelection
             },
             {
                 type: "list",
                 name: "manager",
-                question: "Select the new manager for this employee.",
-                choices: managers
+                message: "Select the new manager for this employee.",
+                choices: employeeRoster
             }
         ]).then((data) => {
             const sql = `
                 UPDATE employee 
-                SET roles_id = ${data.role.value}, manager_id = ${data.manager.value}
-                WHERE id = ${data.employee.value.id}`;
-            const params = employeeUpdates;
+                SET roles_id = ${data.role}, manager_id = ${data.manager}
+                WHERE id = ${data.employee}`;
       
-            db.query(sql, params, (err, result) => {
+            db.query(sql, (err, result) => {
                 if (err) {
                 res.status(400).json({ error: err.message });
                 return;
